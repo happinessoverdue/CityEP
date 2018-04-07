71859810bc4d67cddc7a1aeeecade5e3b16611fb
bitghost <15939175719@163.com>
2018-04-07 13:00:20 +0800

    process只能start()一次，因此修改entrance.py

--- entrance.py
+++ entrance.py
@@ -8,11 +8,22 @@ import Main
 import time
 import os
 
+
 def ucode2utf(conflist):
     rconflist = []
     for us in conflist:
         rconflist.append(us[1].encode('utf-8'))
     return rconflist
+
+
+def prcListInit(solvingList, loginActionTestList, mqList):
+    prclist = []
+    for solvingNo in solvingList:
+        prc = Process(target=Main.solving, args=(
+            loginActionTestList[solvingNo], mqList[solvingNo]))
+        prclist.append(prc)
+    return prclist
+
 
 if __name__ == "__main__":
     # 创建登录连接 LoginActionTest：与camera连接
@@ -39,39 +50,43 @@ if __name__ == "__main__":
         mqList.append(mq)
     print len(mqList)
 
-    solvingList = [0,1,2,3,4]
+    solvingList = [0, 1, 2, 3, 4]
     prcList = []
     for solvingNo in solvingList:
         print loginActionTestList[solvingNo]
         print mqList[solvingNo]
-    for solvingNo in solvingList:
-        prc = Process(target=Main.solving, args=(
-             loginActionTestList[solvingNo], mqList[solvingNo]))
-        prcList.append(prc)
+    # for solvingNo in solvingList:
+    #     prc = Process(target=Main.solving, args=(
+    #         loginActionTestList[solvingNo], mqList[solvingNo]))
+    #     prcList.append(prc)
 
     status = False
     while True:
         nt = time.localtime()
-        if status == False and (nt[3]>=7 and nt[3]<=8 or nt[3]>=11 and nt[3]<=13 or nt[3]>=16 and nt[3]<=17):
+        if status == False and (nt[3] >= 7 and nt[3] <= 8 or nt[3] >= 11 and nt[3] <= 13 or nt[3] >= 16 and nt[3] <= 17):
+            del prcList[:]
+            prcList = prcListInit(solvingList, loginActionTestList, mqList)
             fp = open("pidlist.txt", 'w')
             fp.truncate()
             for i in solvingList:
                 prcList[i].start()
-                fp.write(str(prcList[i].pid)+',')
+                fp.write(str(prcList[i].pid) + ',')
                 print '%d-th processing has been started' % i
             fp.write(str(os.getpid()))
             fp.close()
             status = True
-        elif status == True and not (nt[3]>=7 and nt[3]<=8 or nt[3]>=11 and nt[3]<=13 or nt[3]>=16 and nt[3]<=17):
+        elif status == True and not (nt[3] >= 7 and nt[3] <= 8 or nt[3] >= 11 and nt[3] <= 13 or nt[3] >= 16 and nt[3] <= 17):
             fexist = os.path.exists("pidlist.txt")
             if fexist:
                 os.remove("pidlist.txt")
             for i in solvingList:
                 prcList[i].terminate()
                 print '%d-th processing has been terminated' % i
+            del prcList[:]
             status = False
-        elif not (nt[3]>=7 and nt[3]<=8 or nt[3]>=11 and nt[3]<=13 or nt[3]>=16 and nt[3]<=17):
+        elif not (nt[3] >= 7 and nt[3] <= 8 or nt[3] >= 11 and nt[3] <= 13 or nt[3] >= 16 and nt[3] <= 17):
             fexist = os.path.exists("pidlist.txt")
+            del prcList[:]
             if fexist:
                 os.remove("pidlist.txt")
                 print "has remove the file named pidlist.txt"
--- prc.py
+++ prc.py
@@ -5,18 +5,30 @@ from threading import Timer
 import datetime
 
 
-def ff(c):
-    t = time.time()
-    t = int(t)
-    print t, type(t)
+# def ff(c):
+#     t = time.time()
+#     t = int(t)
+#     print t, type(t)
 
-    while(True):
-        print c
-        c += 1
-        time.sleep(1)
+#     while(True):
+#         print c
+#         c += 1
+#         time.sleep(1)
 
-fp = open("pidlist.txt",'r')
-pid = fp.read()
-pidlist = pid.split(',')
-for pd in pidlist:
-    print type(pd)
+# fp = open("pidlist.txt",'r')
+# pid = fp.read()
+# pidlist = pid.split(',')
+# for pd in pidlist:
+#     print type(pd)
+
+def func(li):
+    print id(li)
+    del li[:]
+    # li = []
+    return li
+
+
+li = [2, 3, 4, 5]
+li = func(li)
+print len(li)
+print id(li)
